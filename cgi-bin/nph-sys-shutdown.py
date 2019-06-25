a4b39556edf6e01683a0ee238c3ce38690e3aec9
kuuen <42200391+kuuen@users.noreply.github.com>
2019-06-25 15:42:14 +0900

    コメント追加

--- cgi-bin/nph-sys-shutdown.py
+++ cgi-bin/nph-sys-shutdown.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-
 import os
 import subprocess
 import multrunchk
 import time
-
 
 # 多重起動チェック
 if multrunchk.chekMultipleRun('nph-sys-shutdown.py', '') == False:
@@ -15,9 +13,9 @@ if multrunchk.chekMultipleRun('nph-sys-shutdown.py', '') == False:
   print('')
   quit()
 
+# nph-sys-reboot.pyと同様visudoに登録が必要
 subprocess.Popen(['sudo', '/sbin/shutdown', '-h', 'now'])
 
 print('HTTP/1.1 302 Found')
 print('Location: ../index.html')
 print('')
-
