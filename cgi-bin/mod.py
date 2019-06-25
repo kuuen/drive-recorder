71e3c05770b5882da165c015fbf75be2b280851b
kuuen <42200391+kuuen@users.noreply.github.com>
2019-06-25 10:23:31 +0900

    コメント追加

--- cgi-bin/mod.py
+++ cgi-bin/mod.py
@@ -7,36 +7,37 @@ import cgi
 
 
 # 起動引数
-
 form = cgi.FieldStorage()
 mvNo = form.getvalue('mv-no','')
 text = ''
 path = '/home/pi/work/DriveRecoder/html/drive/'
+
+# 引数が不正なら処理しない。 ファイル名は数値.mp4の形式
 if (mvNo.isdecimal() == False) :
   text = 'param error no numeric :' + mvNo
 else:
-
+  # 対象のファイルがあるか調べる。なければ処理しない
   if (os.path.exists(path + "{0}.mp4".format(int(mvNo))) == False):
     text = 'param erro nothing number:' + mvNo
   else:
 
-    # セキュリティ
-#    rst = subprocess.Popen(['/home/pi/work/DriveRecoder/cgi-bin/stop.sh'])
+    # パフォーマンスの向上のため録画を停止する。
     rst = subprocess.Popen(['sudo', 'systemctl', 'stop', 'drive_recorder.service'])
 
 
     cmd = "ffmpeg -i " + path + "{0}.mp4 -vcodec copy -acodec -c ".format(int(mvNo)) + path + "{0}new.mp4 -loglevel quiet".format(int(mvNo))
 
     subprocess.call(cmd.split())
-
+    # オリジナルの削除は最後に
     os.rename(path + "{0}.mp4".format(int(mvNo)), path + "{0}old.mp4".format(int(mvNo)))
     os.rename(path + "{0}new.mp4".format(int(mvNo)), path + "{0}.mp4".format(int(mvNo)))
     os.remove(path + "{0}old.mp4".format(int(mvNo)))
 
-
+    # 録画の再開。もともと録画していない場合だとこれはおかしいが気にしない
     rst = subprocess.Popen(['sudo', 'systemctl', 'start', 'drive_recorder.service'])
     text = "ok "
 
+# クライアントへ返す文字列を作成。index.htmlへリダイレクトさせる
 result = '''Content-Type: text/html
 
 <html>
@@ -52,6 +53,6 @@ body {{font-size: 1.4em;}}
 </html>
 '''
 
-
+# クライアントへレスポンス
 print(result.format(text))
 
