6ed0b16192fc0377bcb3e859f7b88b262a481b30
kuuen <42200391+kuuen@users.noreply.github.com>
2019-06-25 10:43:53 +0900

    Update motion-page-init.py

--- cgi-bin/motion-page-init.py
+++ cgi-bin/motion-page-init.py
@@ -7,11 +7,13 @@ import subprocess
 import os
 
 cmd = 'ps aux'
-
+# この挙動に意味がない。削除するか？
 proc = subprocess.Popen(cmd, shell  = True, stdin  = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
 
-
+# motionのページurlを作成。
 url = ''
+# 環境変数からipアドレスを取得
+# 取得できない場合はmotionのストリーミングを表示できないがどうするか？
 if 'HTTP_HOST' in os.environ:
     url = os.environ['HTTP_HOST']
 else:
@@ -24,12 +26,14 @@ print("\n\n")
 
 #data = sys.stdin.read()
 #params = json.loads(data)
+# 無意味な文字列
 text = 'abara'
 
 result = {'text': text,
     'motionurl' : 'http://' + url,
     'screenurl': screenurl  }
 
+# レスポンス
 print(json.JSONEncoder().encode(result))
 print('\n')
 
