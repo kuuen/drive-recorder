9a4df836984c231b37b3716f3f6e1fdf220842d8
kuuen <42200391+kuuen@users.noreply.github.com>
2019-06-25 10:53:59 +0900

    コメント追加

--- cgi-bin/multrunchk.py
+++ cgi-bin/multrunchk.py
@@ -10,6 +10,8 @@ def chekMultipleRun(cmd, service):
     """
     多重コマンド発行防止。指定したコマンドが起動中の場合、サービスが既に稼働中の場合False
     を返す
+    cmd=コマンドの文字列を指定
+    service=サービスの場合はサービス名を指定する
     """
 
     if service == '':
@@ -20,16 +22,22 @@ def chekMultipleRun(cmd, service):
 
     i = 0
     svchk = True
+    # psコマンドの結果を１行づつ取得してチェックする
     for s in  proc.stdout:
+        # utf8として文字列を読まないとうまくいかなかった為だったはず
         str = s.decode('utf-8')
 
+        # 対象のコマンドか？
+        # psコマンド自体も結果に上がるので最後まで確認する
         if str.find(cmd)  != -1 :
             i += 1
 
+        # 対象のサービスか？ヒットしたら既に稼働中なのでループを抜ける
         if str.find(service) != -1 :
             svchk = False
             break
 
+    # 2以上の条件はpsコマンドの結果は省くため
     if i >= 2 or svchk == False:
         return False
     else:
