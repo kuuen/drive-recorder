8b1ce2b75b759eef69e6bdd55a8a197f6ddd21e7
kuuen <42200391+kuuen@users.noreply.github.com>
2019-06-25 10:36:38 +0900

    コメント追加

--- cgi-bin/motion-camera-move.py
+++ cgi-bin/motion-camera-move.py
@@ -59,22 +59,27 @@ def responce(rst) :
   quit() 
 
 
-
 # 多重起動チェック
 if multrunchk.chekMultipleRun('motion-camera-move.py', '') == False:
   responce('ng:作動中')
 
-# 値存在チェックは本来は必要である。そのまま起動するとここで待ちとなる
+# 起動引数を取得
+# httpでリクエストを受けcgiとして起動する想定。コンソールで起動するとここで入力待ちとなってしまう
+# 値存在チェックは本来は必要である。
+# 改善は必要
 data = sys.stdin.read()
 params = json.loads(data)
 move = int(params['move'])
 
+# パラメータの初期化
 pin1 = 0
 pin2 = 0
 pin1i = GPIO.LOW
 pin2i = GPIO.LOW
 moveTime = 0.2
 
+# 起動引数によって処理を分岐。右辺を定数にするべき
+# 1=上　2=下 3＝左　4=右だったはず
 if move == 1 :
   pin1 = PIN_TOP_IN1
   pin2 = PIN_TOP_IN2
@@ -100,6 +105,7 @@ elif move == 4 :
 else:
   responce('ng:param error')
 
+# gpio制御、モータを動かしてカメラの向きを変える
 moveCamera(pin1, pin2, pin1i, pin2i, moveTime)
 
 responce('ok')
