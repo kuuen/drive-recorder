1078edc3ec3318f517b0d90fda58f9a02ded92de
kuuen <42200391+kuuen@users.noreply.github.com>
2019-06-25 11:55:51 +0900

    Update nph-stop.py

--- cgi-bin/nph-stop.py
+++ cgi-bin/nph-stop.py
@@ -4,6 +4,7 @@ import os
 import subprocess
 import multrunchk
 
+# 多重起動チェックの後に録画を止める
 if multrunchk.chekMultipleRun('nph-stop.py', '') == True:
     rst = subprocess.Popen(['sudo', 'systemctl', 'stop', 'drive_recorder.service'])
 
