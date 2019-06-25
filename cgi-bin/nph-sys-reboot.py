3d6c647e053e4a17174d1e0297e5238ac0c217ef
kuuen <42200391+kuuen@users.noreply.github.com>
2019-06-25 11:59:46 +0900

    コメント追加

--- cgi-bin/nph-sys-reboot.py
+++ cgi-bin/nph-sys-reboot.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-
 import os
 import subprocess
 import multrunchk
@@ -15,8 +14,10 @@ if multrunchk.chekMultipleRun('nph-sys-reboot.py', '') == False:
   print('')
   quit()
 
+# 再起動コマンド発行。事前にvisudoに登録しておく必要がある
 subprocess.Popen(['sudo', '/sbin/reboot'])
 
+# 処理結果を返すほうが親切か？
 print('HTTP/1.1 302 Found')
 print('Location: ../index.html')
 print('')
