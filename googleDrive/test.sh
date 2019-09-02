923eff6e4992603af98be096cfb8061787249b71
kuuen <natukyouran@gmail.com>
2019-09-02 21:47:37 +0900

    tes

--- googleDrive/test.sh
+++ googleDrive/test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
+# test
 calltime=$(date)
 
-echo ${calltime} " type:" $1 >> /home/pi/work/python/googleDrive/testdir/log.txt
+echo ${calltime} " type:" $1 >> //home/pi/work/DriveRecoder/googleDrive/testdir/log.txt
 
